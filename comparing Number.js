3ef8a6c86b8d2e5bf49d25b48139cb1545d120ab
Black-Devil007 <singhatmagyan@outlook.com>
2022-02-22 15:41:21 +0530

    javascript

---
+++ comparing Number.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let daysInWeek = 7;
+
+console.log(daysInWeek === 7);
+
+console.log("daysInWeek == 6", daysInWeek == 6);
+
+console.log("daysInWeek < 8", daysInWeek < 8);
+
+console.log(7 == 8);
+
+console.log(7 != 9);
