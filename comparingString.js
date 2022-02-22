6b3a82c5e3acb97950b47179bc3cfa29245af183
Black-Devil007 <singhatmagyan@outlook.com>
2022-02-22 16:18:21 +0530

    javascript

---
+++ comparingString.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let text = 'concatination'
+
+console.log(text == 'concatination');
+
+console.log(text === 'concatination');
+
+console.log(text != 'concat');
+
+console.log(text !== 'concat');
